55a6d29d4389e2f9601df94475420c5df1102161
RichardAvA <44072670+GLoryforRichard@users.noreply.github.com>
2022-06-11 16:50:31 -0400

    Add files via upload

---
+++ assignment2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Assignment1</title>
+    <meta name="Binye Liu Assignmnt 2" content="Binye Liu Assignmnt 2">
+    <meta name="author" content="Binye Liu">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php
+        // Q1  Create a function called file_writer()
+        function file_writer($file_to_write,$content_to_write){
+            echo "";
+        }
+
+        // Q2  Create a function called file_reader() 
+        function file_reader($file_to_read,$num_lines){
+            echo "";
+        }
+
+        // Q3 Create a string called my_content 
+        $my_content = "This is the first line\nThis is the second line\nThis is the third line\n";
+
+        // Q4 Create a string called my_filename
+        $my_filename = "save.txt";
+
+        // Q5 Call your function file_writer() to write the value of my_content into my_filename
+        
+
+        // Q6 Call your function file_reader() to read 3 lines from the file my_filename.
+    ?>
+</body>
+</html>
+
+<?php
+    echo "<hr>";
+    show_source(__FILE__);
+?>
